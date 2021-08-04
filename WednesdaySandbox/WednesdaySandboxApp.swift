bd742749795c5fbb955ce31e2949f9c19dbadda5
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 11:07:11 -0400

    Initial Commit

---
+++ WednesdaySandbox/WednesdaySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  WednesdaySandboxApp.swift
+//  WednesdaySandbox
+//
+//  Created by Russell Gordon on 2021-08-04.
+//
+
+import SwiftUI
+
+@main
+struct WednesdaySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
